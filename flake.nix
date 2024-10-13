af051568c148f238d3190cafd9e09e4ab5938934
conorhk <dev@conorknowles.com>
2024-10-13 23:36:22 +0100

    Include completion

--- flake.nix
+++ flake.nix
@@ -11,16 +11,32 @@
       pkgs = import nixpkgs { inherit system; };
     in {
       packages = {
-        default = pkgs.stdenv.mkDerivation {
+        default = pkgs.stdenvNoCC.mkDerivation rec {
           pname = "script-directory";
-          version = "1.0.0";
+          version = "1.1.0";
           src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.installShellFiles
+            pkgs.patsh
+          ];
+
           installPhase = ''
-            mkdir -p $out/bin
-            cp ./sd $out/bin/sd
-            chmod +x $out/bin/sd
+            runHook preInstall
+            patsh -f sd
+            install -Dt "$out/bin" sd
+            installShellCompletion --zsh _sd
+            runHook postInstall
           '';
-        };
+
+          meta = {
+            description = "A cozy nest for your scripts";
+            homepage = "https://github.com/conorhk/sd";
+            changelog = "https://github.com/conorhk/sd/tree/${src.rev}#changelog";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "sd";
+          };
       };
-    });
+    };
+  });
 }
